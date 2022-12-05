07c8334f96642fb466a122b596afa16155db99a1
nocodepm <nocodepm@gmail.com>
2022-12-06 05:23:59 +0700

    latest build

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration);
 
 const basePromptPrefix =
 `
-Write me an IELTS-style short reading text, written in Bahasa Indonesia. It must contains only 1 paragraph which have included intro, middle, and closing statement. The full text must be less than 250 tokens.
+Write me an IELTS-style short reading text. The text must be written in Bahasa Indonesia. It must contains only 1 main paragraph. The full text must be using less than 250 tokens.
 Topic or Title:
 `;
 const generateAction = async (req, res) => {
