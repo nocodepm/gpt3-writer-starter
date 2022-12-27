d9b251e57ae79f2926089b5f815cff0cfd5464fa
nocodepm <nocodepm@gmail.com>
2022-12-27 22:59:16 +0700

    latest

--- next.config.js
+++ next.config.js
@@ -10,11 +10,4 @@ rewrites: async () => [
   },
 ],
 
-module.exports = {
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  },
-};
+module.exports = nextConfig
